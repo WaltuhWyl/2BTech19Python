e779f4d7a50bf3aadaea7b950dff890e2ce77ca3
WaltuhWyl <114583419+WaltuhWyl@users.noreply.github.com>
2023-09-13 12:12:53 +0000

    test

---
+++ PowtórkaKL2.py
@@ -0,0 +1,29 @@
+#ZAD1
+# a = int(input())
+# b = int(input())
+# print((a**2)+(b**2))
+
+#ZAD2
+# a = int(input())
+# b = int(input())
+# print((a+b)**2)
+
+#ZAD3
+# a = int(input())
+# b = int(input())
+# print((a-b)**3)
+
+#ZAD4
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print(a*b*c)
+
+#ZAD5
+# a = int(input())
+# b = int(input())
+# print(((a+b)*2)/5)
+
+#ZAD6
+# a = int(input())
+# print(a*0.77)
