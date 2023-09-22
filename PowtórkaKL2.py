81f7b50962508738b93f94a8d0a2fe6579100e03
WaltuhWyl <114583419+WaltuhWyl@users.noreply.github.com>
2023-09-22 08:45:55 +0000

    PowtórkaKL2.py update

--- PowtórkaKL2.py
+++ PowtórkaKL2.py
@@ -3,27 +3,103 @@
 # b = int(input())
 # print((a**2)+(b**2))
 
+# def sumakwadratów(a,b):
+#   return ((a**2)+(b**2))
+
+# print(sumakwadratów(a,b))
+
 #ZAD2
 # a = int(input())
 # b = int(input())
-# print((a+b)**2)
+# # print((a+b)**2)
+
+# def kwadratsumy(a,b):
+#   return ((a+b)**2)
+
+# print(kwadratsumy(a,b))
 
 #ZAD3
 # a = int(input())
 # b = int(input())
-# print((a-b)**3)
+# # print((a-b)**3)
+
+# def sześcianróznicy(a,b):
+#   return (a-b)**3
+# print(sześcianróznicy(a,b))
 
 #ZAD4
 # a = int(input())
 # b = int(input())
 # c = int(input())
-# print(a*b*c)
+# # print(a*b*c)
+
+# def iloczyntrzechliczb(a, b, c):
+#   return a*b*c
+# print(iloczyntrzechliczb(a, b, c))
+
 
 #ZAD5
 # a = int(input())
 # b = int(input())
-# print(((a+b)*2)/5)
+# # print(((a+b)*2)/5)
+
+# def dzwadziesciaprocentpodwojonejsumy(a, b):
+#   return(((a+b)*2)/5)
+# print(dzwadziesciaprocentpodwojonejsumy(a, b))
 
 #ZAD6
 # a = int(input())
-# print(a*0.77)
+# # print(a*0.77)
+
+# def netto(a):
+#   return(a*0.77)
+# print(netto(a))
+
+#ZAD7
+# a = int(input())
+# b = int(input())
+# # print(a%b)
+
+# def reszta(a, b):
+#   return a%b
+# print(reszta(a, b))
+
+
+                       #DRUGA KARTA
+#ZAD 1
+
+# a = int(input())
+# def podzielnaprzez3(a):
+#   return a%3 == 0
+# print(podzielnaprzez3(a))
+
+#ZAD 2
+
+# a = int(input())
+# def jesttrzycyfripodzprzez17(a):
+#   return a in range(100, 1000) and a%17 ==0
+# print(jesttrzycyfripodzprzez17(a))
+
+#ZAD3 
+
+# wiek = int(input())
+# def pełnoletni(a):
+#   return a>=18
+# print(pełnoletni(wiek))
+
+#ZAD4
+
+# waga = int(input())
+# limit = 20
+# def limitmostu(a):
+#   return a<=limit
+# print(limitmostu(waga))
+
+#ZAD 5
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# def miescisie(a, b, c):
+#   return a<c<b
+# print(miescisie(a, b, c))
